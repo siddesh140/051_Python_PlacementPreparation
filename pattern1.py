a635c964f1a1a8628855dc6c1477d12363a85cf8
Siddesh Yerawar <siddeshyerawar01@gmail.com>
2026-02-16 00:54:42 +0530

    practice on some new type of patterns.

--- pattern1.py
+++ pattern1.py
@@ -1,3 +1,5 @@
+# https://www.wscubetech.com/resources/python/programs/pattern
+
 # row = 5
 # for i in range(1, row+1):
 #     for j in range(row -i ):
@@ -28,7 +30,7 @@
 #     for j in range(1,i+1):
 #         print(j, end =" ")
 #     print( )
-#  OR
+#  -----------OR----------
 # row = 6
 # for i in range(1, row+1):
 #     for j in range(1, (row-i)+1):
@@ -40,3 +42,46 @@
 # 1 2 3
 # 1 2
 # 1
+
+# row = 5
+# for i in range(1, row+1):
+#     for j in range(1 , i+1):
+#         print(chr(64+j), end = " ")
+#     print("")
+
+# A 
+# A B
+# A B C
+# A B C D
+# A B C D E
+
+# row = 5
+# for i in range(1, row+1):
+#     for j in range(1, (row-i)+2):
+#         print(chr(64+j), end=" ")
+#     print("")
+# -----------OR----------
+# for i in range(row, 0, -1):
+#     for j in range(1, i+1):
+#         print(chr(64+j), end = " ")
+#     print("")
+
+# A B C D E 
+# A B C D
+# A B C
+# A B
+# A
+
+# row = 5
+# num = 1
+# for i in range(1, row+1):
+#     for j in range(1, i+1):
+#         print(num, end = " ")
+#         num += 1
+#     print("")
+
+# 1 
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
