11c29bacd2c708ee11dfc26cdae321ee087667ad
Siddesh Yerawar <siddeshyerawar01@gmail.com>
2026-02-15 23:26:32 +0530

    doing practice on patterns

--- pattern1.py
+++ pattern1.py
@@ -1,8 +1,27 @@
-row = 5
-for i in range(1, row+1):
-    for j in range(row - i):
-        print(" ", end="")
-    for k in range(1,2*i):
-        print("*", end="")
-    print("")
-        
+# row = 5
+# for i in range(1, row+1):
+#     for j in range(row -i ):
+#         print(" ", end = " ")
+#     for k in range(1,2*i):
+#             print("*", end = " ")
+#     print(" ")
+
+# row  = 5
+# for i in range(1, row +1):
+#     for j in range(1,i):
+#         print(j, end=" ")
+#     print(" ")
+
+# row = 5
+# for i in range(row,0,-1):
+#     for j in range(1,i+1):
+#         print(j, end =" ")
+#     print( )
+#  OR
+# row = 6
+# for i in range(1, row+1):
+#     for j in range(1, (row-i)+1):
+#         print(j, end = " ")
+#     print(" ")
+
+https://github.com/siddesh140/PYTHON.git
